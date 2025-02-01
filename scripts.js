d83ff66185a874ade4c9a68434f807750a691421
tarek <tarekdenewar@gmail.com>
2025-02-01 08:36:59 -0500

    editing functionality

--- scripts.js
+++ scripts.js
@@ -2,17 +2,19 @@ const num = document.getElementById("number")
 const fahr = document.getElementById("fahr")
 const cele = document.getElementById("cele")
 result = document.getElementById("result")
-    
 
-function convert(){
-    if (fahr.checked){
+fahr.textContent = "celesius -> fahr"
+cele.textContent = "fahr -> celesius"
+
+function convert() {
+    if (fahr.checked) {
         temp = Number(num.value)
-        temp = temp * 9/5 + 32
+        temp = temp * 9 / 5 + 32
         result.textContent = temp + " fahrenheit"
     }
-    else if (cele.checked){
+    else if (cele.checked) {
         temp = Number(num.value)
-        temp = temp -32 * 5/9
+        temp = temp - 32 * 5 / 9
         result.textContent = temp + " celesius"
     }
 }
